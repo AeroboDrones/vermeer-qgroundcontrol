19202da2bd51bffed802655f8c15ba44e03f331f
czar balangue <czar.balangue@yahoo.com>
2021-11-18 14:24:02 +0000

    VT2: T1 and T2 and a placeholder for T3 is ready

--- src/Vermeer/vermeerloginpage.cpp
+++ src/Vermeer/vermeerloginpage.cpp
@@ -5,10 +5,11 @@
 #include <QFile>
 #include <QString>
 #include <QByteArray>
+#include <QFileInfo>
+
 
 VermeerLogInPage::VermeerLogInPage(QObject *parent) : QObject(parent)
-{
-
+{   
 }
 
 QJsonObject VermeerLogInPage::readJsonFile(QString jsonFilePath)
@@ -32,12 +33,58 @@ QJsonObject VermeerLogInPage::readJsonFile(QString jsonFilePath)
      return document.object();
 }
 
-bool VermeerLogInPage::sendJson(QString filepath)
+void VermeerLogInPage::sendJson(QVariant filepath)
 {
+    QString fileName(filepath.toString());
+    QFile file(fileName);
+    if(!QFileInfo::exists(fileName)){
+        qInfo() << filepath.toString() << ": does not exist";
+        return;
+    }
 
+    if(isConnected)
+    {
+        QString jsonFilePath = filepath.toString();
 
+        QJsonObject jsonObject = readJsonFile(jsonFilePath);
 
+        QJsonDocument doc(jsonObject);
 
+        QString strJson(doc.toJson(QJsonDocument::Compact));
 
-    return true;
+        qInfo() << strJson;
+
+        QByteArray byteArrayJson = strJson.toUtf8();
+
+        QNetworkDatagram datagram(byteArrayJson,QHostAddress(destinationIp),port);
+
+        socket.writeDatagram(datagram);
+    }
+    else
+    {
+         qInfo() << "Connect first";
+    }
+}
+
+void VermeerLogInPage::connectToCompanionComputer(QVariant sourceIpAddress,QVariant destinationIpAddress)
+{
+    sourceIp= sourceIpAddress.toString();
+    destinationIp = destinationIpAddress.toString();
+
+    if(!socket.bind(QHostAddress(sourceIp), port)) {
+        qInfo() << socket.errorString();
+        isConnected = false;
+        return;
+    }
+
+    isConnected = true;
+
+    qInfo() << "Started UDP on" << socket.localAddress() << ":" << socket.localPort();
+}
+
+void VermeerLogInPage::disconnectFromCompanionComputer()
+{
+    socket.close();
+    isConnected = false;
+    qInfo() << "Socket Disconnected";
 }
--- src/Vermeer/vermeerloginpage.h
+++ src/Vermeer/vermeerloginpage.h
@@ -4,25 +4,42 @@
 #include <QObject>
 #include <QDebug>
 
+#include <QVariant>
+
 #include <QJsonDocument>
 #include <QJsonParseError>
 #include <QJsonObject>
 #include <QJsonValue>
 #include <QJsonArray>
 
+#include <QUdpSocket>
+#include <QNetworkDatagram>
+
+#include <QHostAddress>
+#include <QNetworkInterface>
+#include <QAbstractSocket>
+
 class VermeerLogInPage : public QObject
 {
     Q_OBJECT
 public:
     explicit VermeerLogInPage(QObject *parent = nullptr);
-
     QJsonObject readJsonFile(QString jsonFilePath);
 
 signals:
 
 public slots:
 
-    bool sendJson(QString filePath);
+    void sendJson(QVariant filePath);
+    void connectToCompanionComputer(QVariant sourceIpAddress,QVariant destinationIpAddress);
+    void disconnectFromCompanionComputer();
+
+private:
+    QUdpSocket socket;
+    quint16 port{14555};
+    bool isConnected = false;
+    QString sourceIp;
+    QString destinationIp;
 
 };
 
