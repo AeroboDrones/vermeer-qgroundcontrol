940612deafde98e7e18ec560df23415513d061e4
czar balangue <czar.balangue@yahoo.com>
2023-03-22 03:18:05 +0000

    able to display mission by name field and able to parse mission and populate the map - stil need to port some of the features from windows version

--- src/Vermeer/vermeermissionfilemanager.h
+++ src/Vermeer/vermeermissionfilemanager.h
@@ -13,10 +13,7 @@ class VermeerMissionFileManager : public QObject
     Q_OBJECT
 public:
     explicit VermeerMissionFileManager(QObject *parent = nullptr);
-
     QJsonObject filenameToFilePathJson;
-
-
 
 signals:
 
